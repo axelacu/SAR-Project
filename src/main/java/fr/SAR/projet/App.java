b4346dcd8ca38c0bea026912b030e1c0ecfeb61c
axel_ <axel.acuna@dauphine.eu>
2018-12-24 13:18:43 +0100

    firts commit

---
+++ src/main/java/fr/SAR/projet/App.java
@@ -0,0 +1,13 @@
+package fr.SAR.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
