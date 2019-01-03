ae69c0b1da60cb77a583bddec7779a9da1709196
axel_ <axel.acuna@dauphine.eu>
2019-01-03 13:35:36 +0100

    Lelann

---
+++ src/main/java/fr/SAR/projet/electionLelan/RequeteLelann.java
@@ -0,0 +1,17 @@
+package fr.SAR.projet.electionLelan;
+
+public class RequeteLelann {
+    private int chef;
+
+    public RequeteLelann(int K) {
+        this.chef = K;
+    }
+
+    public int getSiteId() {
+        return chef;
+    }
+
+    public void setSiteId(int siteId) {
+        this.chef = siteId;
+    }
+}
